80d52cea141393ceb90168084733c807716364c1
Tóth Dániel <91130208+tothdani561@users.noreply.github.com>
2026-02-01 08:33:26 +0100

    first commit

---
+++ src/main/java/dev/hytalemodding/ExamplePlugin.java
@@ -0,0 +1,39 @@
+package dev.hytalemodding;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import dev.hytalemodding.commands.*;
+import dev.hytalemodding.events.ExampleEvent;
+
+import javax.annotation.Nonnull;
+
+public class ExamplePlugin extends JavaPlugin {
+
+    public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("[STOKEYNPCS] ExamplePlugin is initializing!");
+        commands();
+        events();
+    }
+
+    private void events() {
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
+    }
+
+    private void commands() {
+        this.getCommandRegistry().registerCommand(new ExampleCommand("example", "An example command"));
+        this.getCommandRegistry().registerCommand(new ServerRulesCommand());
+        this.getCommandRegistry().registerCommand(new AbsPlayerCommand());
+        this.getCommandRegistry().registerCommand(new AbsTargetPlayerHealCommand());
+        this.getCommandRegistry().registerCommand(new AbsTargetEntityCommand());
+        this.getCommandRegistry().registerCommand(new AbsTargetEntityCommand());
+    }
+}
